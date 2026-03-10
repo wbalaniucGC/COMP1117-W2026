157467fcef9bd85abf6dc0eb79e9b0cc37912e74
wbalaniucGC <113048699+wbalaniucGC@users.noreply.github.com>
2026-03-10 13:28:56 -0400

    Week 9 template ready

--- Assets/Scripts/Mechanics/Save/PlayerSaveData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class PlayerSaveData
-{
-    // When using JsonUtility, we must use simple data types
-    public float[] position = new float[3];
-}
