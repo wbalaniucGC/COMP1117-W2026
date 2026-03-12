944bcee8f8a018b19da5defc362026b0de706796
wbalaniucGC <113048699+wbalaniucGC@users.noreply.github.com>
2026-03-12 13:20:59 -0400

    Week 9 Base

--- Assets/Scripts/Mechanics/Save/PlayerSaveData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class PlayerSaveData
-{
-    // When using JsonUtility, we must use SIMPLE data types
-    // public Vector3 position;    // (float, float, float)
-    public float[] position = new float[3];
-}
--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -9,10 +9,6 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] private Image fader;
     [SerializeField] private GameObject menuPanel;
 
-    [Header("Checkpoint References")]
-    [SerializeField] private SaveManager saveManager;
-    [SerializeField] private GameObject player;
-
     [Header("Settings")]
     [SerializeField] private float fadeDuration = 1.0f;
 
@@ -21,52 +17,6 @@ public class GameOverUI : MonoBehaviour
         // Disable menu panel. Set fader to be transparent.
         menuPanel.SetActive(false);
         fader.color = new Color(0, 0, 0, 0);
-    }
-
-    public void LoadFromCheckpoint()
-    {
-        // 1. Read the data from the Save Manager
-        Vector3 playerPos = saveManager.LoadGame();
-
-        // Safety check!
-        if(playerPos != null)
-        {
-            // 2. Reactivate the player.
-            player.SetActive(true);
-
-            // 3. Set the player back to the checkpoint position and reset values
-            player.transform.position = playerPos;
-
-            Player playerScript = player.GetComponent<Player>();
-            if (playerScript != null)
-            {
-                playerScript.ResetState();
-            }
-
-            // 4. Reset velocity so they don't spawn with "death" physics
-            Rigidbody2D rBody = player.GetComponent<Rigidbody2D>();
-            if (rBody != null)
-            {
-                rBody.linearVelocity = Vector2.zero;
-                rBody.gravityScale = 5f;
-                rBody.simulated = true;
-            }
-
-            // 5. Reset collisions
-            Collider2D col = player.GetComponent<Collider2D>();
-            if(col != null)
-            {
-                col.enabled = true;
-            }
-
-            // 6. Hide the game over menu and reset values
-            menuPanel.SetActive(false);
-            fader.color = new Color(0, 0, 0, 0);
-        }
-        else
-        {
-            Debug.LogError("No checkpoint file found");
-        }
     }
 
     public void ShowGameOver()
